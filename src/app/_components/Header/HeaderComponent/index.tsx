a3b9f9e495253b1f31571f877efb4ec2acd39e6c
inbumbaek <inbumbaek@gmail.com>
2024-01-18 15:40:46 -0800

    save data

---
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Header } from '../../../../payload/payload-types'
+import { Gutter } from '../../Gutter'
+import Link from 'next/link'
+
+const HeaderComponent = ({ header }: { header: Headerder }) => {
+  return (
+    <nav>
+      <Gutter>
+        <Linkk>
+        </Link>
+      </Gutter>
+    </nav>
+  )
+}
+
+export default HeaderComponent
