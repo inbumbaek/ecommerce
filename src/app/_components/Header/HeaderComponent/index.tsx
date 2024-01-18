150447974555da32e489d5aa6b971a0af5221ef2
inbumbaek <inbumbaek@gmail.com>
2024-01-18 15:50:44 -0800

    add img

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,11 +3,12 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 
-const HeaderComponent = ({ header }: { header: Headerder }) => {
+const HeaderComponent = ({ header }: { header: Header }) => {
   return (
     <nav>
       <Gutter>
-        <Linkk>
+        <Link href="/">
+          <img src="/logo-black.svg" alt="logo" />
         </Link>
       </Gutter>
     </nav>
