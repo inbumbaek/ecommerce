de6bd0a79a80552bfabba32967216f767f51178c
inbumbaek <inbumbaek@gmail.com>
2024-01-19 22:56:07 -0800

    add header and wrap

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
+import classes from './index.module.scss'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
-    <nav>
-      <Gutter>
+    <nav className={[classes.header]}>
+      <Gutter className={classes.wrap}>
         <Link href="/">
           <img src="/logo-black.svg" alt="logo" />
         </Link>
