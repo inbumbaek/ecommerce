71301f115805b2232eebb29bf20ee9e8b2d4f582
inbumbaek <inbumbaek@gmail.com>
2024-01-20 23:28:44 -0800

    add mobilenav

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -3,14 +3,20 @@ import { Header } from '../../../../payload/payload-types'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
 import classes from './index.module.scss'
+import Image from 'next/image'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../MobileNav'
 
 const HeaderComponent = ({ header }: { header: Header }) => {
   return (
     <nav className={[classes.header]}>
       <Gutter className={classes.wrap}>
         <Link href="/">
-          <img src="/logo-black.svg" alt="logo" className={classes.logo}/>
+          <Image src="/logo-black.svg" alt="logo" width={170} height={50} />
         </Link>
+
+        <HeaderNav header={header} />
+        <MobileNav header={header} />
       </Gutter>
     </nav>
   )
