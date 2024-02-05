95025ab420c9d53d5660037072fc2435eab1fc5e
inbumbaek <inbumbaek@gmail.com>
2024-02-05 14:23:13 -0800

    add image

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -29,6 +29,16 @@ export default async function Login() {
           />
         </Link>
       </div>
+      <div className={classes.formWrapper}>
+        <div className={classes.formContainer}>
+          <RenderParams className={classes.params} />
+
+          <div className={classes.formTitle}>
+            <h3>Welcome</h3>
+            <Image src="/assets/icons/hand.png" alt="hand" width={30} height={30} />
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
