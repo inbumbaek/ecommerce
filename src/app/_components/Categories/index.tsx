530aeeff1cefcd3c160714abec664a7a329ff41b
inbumbaek <inbumbaek@gmail.com>
2024-02-23 23:38:22 -0800

    work on categories

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import classes from './index.module.scss'
 
 const Categories = () => {
   return (
-    <section>
+    <section className={classes.container}>
+      <div className={classes.titleWrapper}>
+        <h3>Shop by Categories</h3>
+        <Link href="/products">Show All</Link>
+      </div>
       
+      <div className={classes.list}>
+
+      </div>
     </section>
   )
 }
