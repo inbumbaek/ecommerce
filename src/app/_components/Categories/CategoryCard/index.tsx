879faa98a72f7f18e1c02d5311fd6177c19b3053
inbumbaek <inbumbaek@gmail.com>
2024-02-25 21:10:33 -0800

    add image

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -8,8 +8,10 @@ type CategoryCardProps ={
 }
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
+  const media = category.media as Media;
+
   return (
-    <Link href="/products" className={classes.card}>
+    <Link href="/products" className={classes.card} style={{ backgroundImage: `url(${media.url})`}}>
       <p className={classes.title}>{category.title}</p>
     </Link>
   )
