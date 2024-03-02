99f0c221e6b780f6b91b34df7c94c201344ba297
inbumbaek <inbumbaek@gmail.com>
2024-03-01 23:40:41 -0800

    add promotion

--- src/app/(pages)/[slug]/page.tsx
+++ src/app/(pages)/[slug]/page.tsx
@@ -35,7 +35,7 @@ export default async function Page({ params: { slug = 'home' } }) {
       slug,
       draft: isDraftMode,
     })
-    
+
     categories = await fetchDocs<Category>('categories')
   } catch (error) {
     // when deploying this template on Payload Cloud, this page needs to build before the APIs are live
@@ -59,23 +59,23 @@ export default async function Page({ params: { slug = 'home' } }) {
 
   return (
     <React.Fragment>
-    {slug === 'home' ? (
-      <section>
-        <Hero {...hero} />
-        <Gutter className={classes.home}>
-          <Categories categories={categories}/>
-        </Gutter>
-      </section>
-    ) : (
-      <>
-      <Hero {...hero} />
-      <Blocks
-        blocks={layout}
-        disableTopPadding={!hero || hero?.type === 'none' || hero?.type === 'lowImpact'}
-      />
-      </>
-    )}
-      
+      {slug === 'home' ? (
+        <section>
+          <Hero {...hero} />
+          <Gutter className={classes.home}>
+            <Categories categories={categories} />
+            <Promotion />
+          </Gutter>
+        </section>
+      ) : (
+        <>
+          <Hero {...hero} />
+          <Blocks
+            blocks={layout}
+            disableTopPadding={!hero || hero?.type === 'none' || hero?.type === 'lowImpact'}
+          />
+        </>
+      )}
     </React.Fragment>
   )
 }
