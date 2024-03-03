98550858a072fb45fbd44a117cd364e771040aa9
inbumbaek <inbumbaek@gmail.com>
2024-03-02 23:41:22 -0800

    working on promotion

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import classes from './index.module.scss'
 
 const Promotion = () => {
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  })
+
   return (
     <section className={classes.promotion}>
       <div className={classes.textBox}>
         <h3 className={classes.title}>Deals of the Month</h3>
-        <p>Get ready for a shopping experience like never before with our Deals of the Month! Every
+        <p>
+          Get ready for a shopping experience like never before with our Deals of the Month! Every
           purchase comes with exclusive perks and offers, making this month a celebration of savvy
-          choices and amazing deals. Don't miss out! 🎁🛒</p>
-          
-      </div>
+          choices and amazing deals. Don't miss out! 🎁🛒
+        </p>
 
+        <ul className={classes.stats}></ul>
+          <StatBox label="Days" value="" />
+      </div>
     </section>
   )
 }
+
+const StatBox = ({ label, value }: { label: string; value: number }) => (
+  <li className={classes.statBox}>
+    <h4>{value}</h4>
+    <p>{label}</p>
+  </li>
+)
 
 export default Promotion
