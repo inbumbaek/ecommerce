168cc216b99b3a7b42a3d2d09851144cedcf9b16
inbumbaek <inbumbaek@gmail.com>
2024-03-02 23:17:29 -0800

    working on promotion

--- src/app/_components/Promotion/index.tsx
+++ src/app/_components/Promotion/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import classes from './index.module.scss'
 
 const Promotion = () => {
   return (
-    <div>Promotion</div>
+    <section className={classes.promotion}>
+      <div className={classes.textBox}>
+        <h3 className={classes.title}>Deals of the Month</h3>
+        <p>Get ready for a shopping experience like never before with our Deals of the Month! Every
+          purchase comes with exclusive perks and offers, making this month a celebration of savvy
+          choices and amazing deals. Don't miss out! 🎁🛒</p>
+          
+      </div>
+
+    </section>
   )
 }
 
