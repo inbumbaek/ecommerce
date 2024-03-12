20f47c14d166a3df8b6377b482dc435a002ba67f
inbumbaek <inbumbaek@gmail.com>
2024-03-11 23:20:43 -0700

    add categories

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
+import { Category } from '../../../../payload/payload-types'
 
-const Filters = () => {
+const Filters = ({ categoreis }: { categories: Category[] }) => {
+  const { categoryFilters, sort, setCategoryFilters, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {
+
+  }
   return (
     <div className={classes.filters}>
       <div>
         <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map((category) => {
+            const isSelected= false;
+
+          })}
+        </div>
       </div>
     </div>
   )
