0820d2316c0aee8fc13dc13a5c1732650cc429fe
inbumbaek <inbumbaek@gmail.com>
2024-04-15 16:16:11 -0700

    add Price

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import classes from './index.module.scss'
 import Link from 'next/link'
 import { Media } from '../../../_components/Media'
+import { Price } from '../../../_components/Price'
 
 const CartItem = ({ product, title, metaImage, qty, addItemToCart, index }) => {
   const [quantity, setQuantity] = useState(qty)
@@ -18,7 +19,14 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart, index }) => {
         {metaImage && typeof metaImage !== 'string' && (
           <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
         )}
-      </Link> 
+      </Link>
+
+      <div className={classes.itemDetails}>
+        <div className={classes.titleWrapper}>
+          <h6>{title}</h6>
+          <Price product={product} button={false} />
+        </div>
+      </div>
     </li>
   )
 }
