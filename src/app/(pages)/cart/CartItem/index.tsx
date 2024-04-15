3252f18dd2b58dd8bd98a57ec36e80044f318b6a
inbumbaek <inbumbaek@gmail.com>
2024-04-14 23:57:23 -0700

    add to CartItem

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import classes from './index.module.scss'
 
 const CartItem = ({ product, title, metaImage, qty, addItemToCart, index }) => {
+  const [quantity, setQuantity] = useState(qty)
+  const decrementQty = () => {}
+  const incrementQty = () => {}
+  const enter Qty = () => {}
+
   return (
     <li className={classes.item} key={title}>
       <h6>TITLE</h6>
