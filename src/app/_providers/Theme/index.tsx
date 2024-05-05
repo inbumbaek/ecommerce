a5667a9bc472cf0c077aaf2aa7c6755404c58ff9
inbumbaek <inbumbaek@gmail.com>
2024-05-04 23:48:37 -0700

    save

--- src/app/_providers/Theme/index.tsx
+++ src/app/_providers/Theme/index.tsx
@@ -49,7 +49,7 @@ export const ThemeProvider: React.FC<{ children?: React.ReactNode }> = ({ childr
     setThemeState(defaultTheme)
   }, [])
 
-  return <ThemeContext.Provider value={{ theme, setTheme }}>{children}</ThemeContext.Provider>
+  return <ThemeContext.Provider value={{ theme: defaultTheme, setTheme }}>{children}</ThemeContext.Provider>
 }
 
 export const useTheme = (): ThemeContextType => useContext(ThemeContext)
