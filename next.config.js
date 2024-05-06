25f452ba68fe8f8f8c0fdb904d94116c4bb71bf7
inbumbaek <inbumbaek@gmail.com>
2024-05-05 23:54:18 -0700

    nextConfig

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
 const nextConfig = {
+  typescrit: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
